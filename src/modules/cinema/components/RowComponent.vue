1ff905f11b850e18de939c748fb663b6c2152539
jaserleney <yaser.keny@gmail.com>
2023-04-21 10:00:19 -0500

    Before test

--- src/modules/cinema/components/RowComponent.vue
+++ src/modules/cinema/components/RowComponent.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="container">
+  <div class="container bg-dark rounded p-3">
     <div class="row justify-content-md-center">
-      <img src="../assets/images/pantalla.png" alt="screen" class="img-fluid col-12" />
+      <img src="../assets/images/pantalla.png" alt="screen" class="img-fluid col-12 screen" />
       <div class="container col-10">
-        <div class="row justify-content-center align-items-center gap-5">
-          <div class="row justify-content-md-center align-items-center gap-2">
+        <div class="row grid-fluid justify-content-center align-items-center gap-5">
+          <div class="row justify-content-center align-items-center gap-2">
             <SeatComponet v-for="seat in 20" />
           </div>
-          <div class="row justify-content-md-center align-items-center gap-2">
+          <div class="row justify-content-center align-items-center gap-2">
             <SeatComponet v-for="seat in 10" />
           </div>
-          <div class="row justify-content-md-center align-items-center gap-2">
+          <div class="row justify-content-center align-items-center gap-2">
             <SeatComponet v-for="seat in 25" />
           </div>
         </div>
@@ -34,5 +34,15 @@ export default {
 <style scoped>
 .row {
   white-space: nowrap;
+}
+
+.grip-fluid {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  text-align: center;
+}
+
+.screen {
+  filter: drop-shadow(10px 10px 50px #e5e4e5);
 }
 </style>
--- src/modules/cinema/components/SeatComponet.vue
+++ src/modules/cinema/components/SeatComponet.vue
@@ -27,7 +27,6 @@ export default {
 .seat:hover {
   transform: scale(1.2);
   filter: grayscale(100%) brightness(40%) sepia(100%) hue-rotate(-50deg) saturate(1000%);
-
   filter: grayscale(100%) brightness(40%) sepia(100%) hue-rotate(90deg) saturate(1000%);
 }
 </style>
