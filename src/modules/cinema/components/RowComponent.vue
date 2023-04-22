ebae02e6aef0f37e76016065a39c4190520e0c17
jaserleney <yaser.keny@gmail.com>
2023-04-22 11:02:41 -0500

    push

--- src/modules/cinema/components/RowComponent.vue
+++ src/modules/cinema/components/RowComponent.vue
@@ -23,30 +23,30 @@
 </template> -->
 
 <template>
-  <div class="container main my-5">
+  <div class="container main col-md-9 pb-3 pt-4">
     <div class="row justify-content-md-center">
-      <img src="../assets/images/pantalla.png" alt="screen" class="img-fluid col-12 screen" />
+      <img src="../assets/images/pantalla.png" alt="screen" class="img-fluid w-100 screen" />
       <div class="d-flex flex-md-row flex-column gap-5 gap-md-2">
         <div
-          class="container d-flex flex-wrap justify-content-between align-items-start gap-1 align-content-start"
+          class="container d-flex flex-wrap justify-content-start align-items-start gap-1 align-content-start"
         >
-          <SeatComponet v-for="seat in 30" />
+          <SeatComponet v-for="seat in 36" />
         </div>
         <div
-          class="container d-flex flex-wrap justify-content-between align-items-start gap-1 align-content-start"
+          class="container d-flex flex-wrap justify-content-start align-items-start gap-1 align-content-start content-box"
         >
-          <SeatComponet v-for="seat in 15" />
+          <SeatComponet v-for="seat in 10" />
         </div>
         <div
-          class="container d-flex flex-wrap justify-content-between align-items-start gap-1 align-content-start"
+          class="container d-flex flex-wrap justify-content-start align-items-start gap-1 align-content-start"
         >
-          <SeatComponet v-for="seat in 35" />
+          <SeatComponet v-for="seat in 25" />
         </div>
       </div>
     </div>
-    <div class="container max-auto text-center exit">
+    <!-- <div class="container max-auto text-center exit">
       <img src="../assets/images/exit.png" alt="exit" class="img-fluid" />
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -64,34 +64,31 @@ export default {
   white-space: nowrap;
 }
 
-/* .grip-fluid {
+/* .grid-fluid {
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(45px, 1fr));
   text-align: center;
 } */
 
 .screen {
-  filter: drop-shadow(0px 30px 50px #e3e4e5);
+  filter: drop-shadow(0rem 1rem 1rem #e3e4e5);
 }
 
 .main {
   background-color: #2b2b2b;
   border-radius: 0.5rem;
   position: relative;
-  padding: 1rem;
-  padding-bottom: 10rem;
 }
 
-.main .exit {
+.content-box {
+  position: relative;
+}
+.content-box:after {
+  content: "";
   position: absolute;
-  top: 90%;
-  left: 0;
-  margin: 0 auto !important;
-  padding: 0 !important;
-}
-
-.exit > img {
-  margin: 0 auto !important;
-  padding: 0 !important;
+  width: 100%;
+  bottom: 0%;
+  height: 2rem;
+  background-color: aqua;
 }
 </style>
