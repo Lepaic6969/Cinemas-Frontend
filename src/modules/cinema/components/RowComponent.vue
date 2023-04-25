f62c562bb44b059580d2cabe7df547434fb63d6b
jaserleney <yaser.keny@gmail.com>
2023-04-25 09:22:40 -0500

    Before merged

--- src/modules/cinema/components/RowComponent.vue
+++ src/modules/cinema/components/RowComponent.vue
@@ -99,7 +99,7 @@ export default {
 
 /* .grid-fluid {
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(45px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
   text-align: center;
 } */
 
