fbadb1318d8211cb33154ae7a1703d7e9187ceba
Marin28c1997 <96443496+Marin28c1997@users.noreply.github.com>
2023-04-25 11:21:37 -0500

    cinema enpoints

--- src/modules/home/views/HomeView.vue
+++ src/modules/home/views/HomeView.vue
@@ -1,19 +1,19 @@
 <template>
-  <Nav/>
+  <Nav />
 
   <n-row>
     <n-col v-for="movie in movies" :key="movie.id" :span="5">
-      <n-card class="text-center" :title="movie.name">
+      <n-card class="text-center " style="background:#000" :title="movie.name" >
         <div class="card-image">
-               <img
-      :src="
-        movie.image
-          ? movie.image.secure_url
-          : 'https://tradebharat.in/assets/catalogue/img/no-product-found.png'
-      "
-      class="card-img-top"
-      :alt="movie.name"
-    />
+          <img
+            :src="
+              movie.image
+                ? movie.image.secure_url
+                : 'https://tradebharat.in/assets/catalogue/img/no-product-found.png'
+            "
+            class="card-img-top"
+            :alt="movie.name"
+          />
           <div class="card-text">
             <n-button type="success">Trailer</n-button>
             <n-button type="info" @click="buy()">Ver más</n-button>
@@ -21,9 +21,9 @@
         </div>
       </n-card>
     </n-col>
-  </n-row>  
+  </n-row>
 
-    <!-- <n-row>
+  <!-- <n-row>
     <n-col v-for="movie in movies" :key="movie.id" :span="5">
       <n-card class="text-center" :title="movie.name">
         <div class="card-image">
@@ -61,7 +61,7 @@ export default {
   components: {
     Nav,
     Footer,
-    Modal
+    Modal,
   },
   data() {
     return {
@@ -69,15 +69,18 @@ export default {
       showLoginModal: false,
     };
   },
-    methods: {
-    
-    buy(){
-      this.showLoginModal = !this.showLoginModal;
-    }
-    
+  methods: {
+    buy() {
+      const token = localStorage.getItem("token");
+      if (token) {
+        this.$router.push("/user/poster");
+      } else {
+        this.showLoginModal = true;
+      }
+    },
   },
   async mounted() {
-     try {
+    try {
       const data = await fetchData("/movies");
       this.movies = data.body;
       console.log("Peliculas:", this.movies); // Agregado
