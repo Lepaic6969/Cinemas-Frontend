ec6c2b04e749a8ffde054d1f562489eba0f82999
jaserleney <yaser.keny@gmail.com>
2023-04-28 12:08:10 -0500

    Animation screen ready

--- src/modules/tickets/components/RowComponent.vue
+++ src/modules/tickets/components/RowComponent.vue
@@ -85,9 +85,9 @@ export default {
 }
 
 .screen {
+  opacity: 1;
   filter: drop-shadow(0rem 1rem 1rem #e3e4e5) contrast(120%) brightness(110%);
   animation: onScreen 3s ease-in-out 1;
-  opacity: 0.9;
 }
 
 @keyframes onScreen {
